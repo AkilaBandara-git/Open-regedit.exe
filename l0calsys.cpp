1006d1512b992475982bc2bac538d394f1bfb1f9
AkilaBandara <59506472+AkilaBandara-git@users.noreply.github.com>
2020-04-04 07:28:57 -0700

    Create l0calsys.cpp

---
+++ l0calsys.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	system("cls");
+	cout << "[i]Author: Akila Bandara\n";
+	cout << "[i]Follow: twitter.com/akilaontweet\n";
+	cout << "Opening regedit......";
+	system("c:\\windows\\regedit.exe");
+}
